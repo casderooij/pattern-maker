c402fde6590bc96a8fcb7391b99033c53f8b59cf
casderooij <hello@casderooij.nl>
2022-03-09 09:45:03 +0100

    Update to latest version of svelte-kit

---
+++ .svelte-kit/generated/manifest.js
@@ -0,0 +1,21 @@
+const c = [
+	() => import("../../src/routes/__layout.svelte"),
+	() => import("../runtime/components/error.svelte"),
+	() => import("../../src/routes/index.svelte"),
+	() => import("../../src/routes/tool/__layout.svelte"),
+	() => import("../../src/routes/tool/index.svelte")
+];
+
+const d = decodeURIComponent;
+
+export const routes = [
+	// src/routes/index.svelte
+	[/^\/$/, [c[0], c[2]], [c[1]]],
+
+	// src/routes/tool/index.svelte
+	[/^\/tool\/?$/, [c[0], c[3], c[4]], [c[1]]]
+];
+
+// we import the root layout/error components eagerly, so that
+// connectivity errors after initialisation don't nuke the app
+export const fallback = [c[0](), c[1]()];
